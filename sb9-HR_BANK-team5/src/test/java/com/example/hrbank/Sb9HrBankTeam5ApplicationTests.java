76fc1e72aacf943b293d2f9a60bb32acba4fde59
seunghyeonjeon <seunghyeonjeon57@gmail.com>
2026-03-11 15:42:52 +0900

    Chore:Finalize project structure and database configuration

---
+++ sb9-HR_BANK-team5/src/test/java/com/example/hrbank/Sb9HrBankTeam5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hrbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb9HrBankTeam5ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
