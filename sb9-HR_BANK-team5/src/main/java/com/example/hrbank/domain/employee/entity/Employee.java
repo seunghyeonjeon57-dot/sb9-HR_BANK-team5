38fd26f7b4b40b075bfb17dd5826ba8aeafac634
seunghyeonjeon <seunghyeonjeon57@gmail.com>
2026-03-12 13:10:51 +0900

    feat:사원 엔티티 구성

---
+++ sb9-HR_BANK-team5/src/main/java/com/example/hrbank/domain/employee/entity/Employee.java
@@ -0,0 +1,71 @@
+package com.example.hrbank.domain.employee.entity;
+
+import com.example.hrbank.domain.employee.entity.enums.EmployeeStatus;
+import com.example.hrbank.global.entity.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table
+@Entity(name = "employee")
+@Getter
+@Setter
+public class Employee extends BaseTimeEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  @Column(name ="name",length = 50,nullable = false)
+  private String name;
+  @Column(name = "email",length=100,nullable = false,unique = true)
+  private String email;
+  @Column(name="employee_number",length =50,nullable = false,unique = true)
+  private String employeeNumber;
+  @Column(name="position",length = 50)
+  private String position;
+  @Column(nullable = false)
+  private LocalDate hireDate;
+
+  @Enumerated(EnumType.STRING)
+  @Column(name="status",nullable = false,columnDefinition = "employee_status")
+  private EmployeeStatus status;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name="department_id")
+  private Department department;
+
+  @OneToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name="profileImage_id")
+  private BinaryContent profileImageId;
+
+
+  public Employee(Long id, String name, String email, String employeeNumber, String position,
+      LocalDate hireDate, EmployeeStatus status, Department department,
+      BinaryContent profileImageId) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.employeeNumber = employeeNumber;
+    this.position = position;
+    this.hireDate = hireDate;
+    this.status = status;
+    this.department = department;
+    this.profileImageId = profileImageId;
+  }
+}
---
+++ sb9-HR_BANK-team5/src/main/java/com/example/hrbank/domain/employee/entity/enums/EmployeeStatus.java
@@ -0,0 +1,11 @@
+package com.example.hrbank.domain.employee.entity.enums;
+
+
+public enum EmployeeStatus {
+  ACTIVE,
+  ON_LEAVE,
+  RESIGNED
+
+
+
+}
