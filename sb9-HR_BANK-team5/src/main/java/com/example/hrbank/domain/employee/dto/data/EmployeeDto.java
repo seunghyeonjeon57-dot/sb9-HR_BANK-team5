c92ff3a7cb06c184a42bfedc5114639ec0a4c26f
seunghyeonjeon <seunghyeonjeon57@gmail.com>
2026-03-12 13:42:09 +0900

    feat: dto구성

---
+++ sb9-HR_BANK-team5/src/main/java/com/example/hrbank/domain/employee/dto/data/EmployeeDto.java
@@ -0,0 +1,5 @@
+package com.example.hrbank.domain.employee.dto.data;
+
+public class EmployeeDto {
+
+}
---
+++ sb9-HR_BANK-team5/src/main/java/com/example/hrbank/domain/employee/dto/request/EmployeeCreateRequest.java
@@ -0,0 +1,5 @@
+package com.example.hrbank.domain.employee.dto.request;
+
+public class EmployeeCreateRequest {
+
+}
