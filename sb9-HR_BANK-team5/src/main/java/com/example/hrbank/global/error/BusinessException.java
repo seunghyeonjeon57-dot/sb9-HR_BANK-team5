0550b5d4a00be878d9b5f6e80b4f14791bf07f29
geumbi <gkrry0404@naver.com>
2026-03-12 11:23:13 +0900

    first commit/ feat: department entity 생성

--- sb9-HR_BANK-team5/src/main/java/com/example/hrbank/global/error/BusinessException.java
+++ sb9-HR_BANK-team5/src/main/java/com/example/hrbank/global/error/BusinessException.java
@@ -5,9 +5,9 @@ import lombok.Getter;
 //서비스 로직에서 사용하는 최상위 예외클래스
 @Getter
 public class BusinessException extends RuntimeException {
-  private final ErrorCode errorCode;
+  private final com.example.hrbank.global.error.ErrorCode errorCode;
 
-  public BusinessException(ErrorCode errorCode) {
+  public BusinessException(com.example.hrbank.global.error.ErrorCode errorCode) {
     super(errorCode.getMessage());
     this.errorCode = errorCode;
   }
