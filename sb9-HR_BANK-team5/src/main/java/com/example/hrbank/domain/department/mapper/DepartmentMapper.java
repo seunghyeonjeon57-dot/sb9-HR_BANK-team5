edebf41cfa7bc5cf67846329959c8f7f89e7ba3e
geumbi <gkrry0404@naver.com>
2026-03-15 18:33:10 +0900

    feat: repo, service 미완성

--- sb9-HR_BANK-team5/src/main/java/com/example/hrbank/domain/department/mapper/DepartmentMapper.java
+++ sb9-HR_BANK-team5/src/main/java/com/example/hrbank/domain/department/mapper/DepartmentMapper.java
@@ -1,12 +1,26 @@
 package com.example.hrbank.domain.department.mapper;
 
 import com.example.hrbank.domain.department.dto.data.DepartmentDto;
+import com.example.hrbank.domain.department.dto.response.CursorPageResponseDepartmentDto;
 import com.example.hrbank.domain.department.entity.Department;
+import java.util.Base64;
+import java.util.List;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface DepartmentMapper {
 
   DepartmentDto toDto(Department department);
+  List<DepartmentDto> toDtoList(List<Department> entities);
+  default CursorPageResponseDepartmentDto toCursorPageDto(List<Department> entities, Integer size, Long totalElements, boolean hasNext) {
+    List<DepartmentDto> content = toDtoList(entities);
+    Long lastId = entities.isEmpty() ? null : entities.get(entities.size()-1).getId();
+    String encodedCursor = null;
+    if (lastId != null) {
+      encodedCursor = Base64.getEncoder().encodeToString(String.valueOf(lastId).getBytes());
+    }
 
+
+    return new CursorPageResponseDepartmentDto(content, encodedCursor,lastId, size, totalElements, hasNext);
+  }
 }
